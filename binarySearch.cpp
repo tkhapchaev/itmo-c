a5af39a3a2745fece90ed5a15f8416fa94c9f5b0
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2021-12-10 18:10:59 +0300

    UPD: binarySearch.cpp

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -1,81 +1,83 @@
 #include <iostream>
 using namespace std;
 
-int myBinarySearch (int A[], int Number, int key) {
-    int left = -1;
-    int right = Number;
-    int middle = 0;
-    while (left < right - 1) {
-        middle = (left + right) / 2;
-        if (A[middle] < key) {
-            left = middle;
-        }
-        else {
-            right = middle;
-        }
+int myBinarySearch(int A[], int Number, int key) {
+  int left = -1;
+  int right = Number;
+  int middle = 0;
+  while (left < right - 1) {
+    middle = (left + right) / 2;
+    if (A[middle] < key) {
+      left = middle;
+    } else {
+      right = middle;
     }
+  }
 
-    if (right < Number && A[right] == key) {
-        return right + 1;
-    }
-
-    else {
-        return -1;
-    }
+  if (right < Number && A[right] == key) {
+    return right + 1;
+  } else {
+    return -1;
+  }
 }
 
-int myBinarySearchLast (int A[], int Number, int key) {
-    int left = -1;
-    int right = Number;
-    int middle = 0;
-    while (left < right - 1) {
-        middle = (left + right) / 2;
-        if (A[middle] <= key) {
-            left = middle;
-        }
-        else {
-            right = middle;
-        }
+int myBinarySearchLast(int A[], int Number, int key) {
+  int left = -1;
+  int right = Number;
+  int middle = 0;
+  while (left < right - 1) {
+    middle = (left + right) / 2;
+    if (A[middle] <= key) {
+      left = middle;
+    } else {
+      right = middle;
     }
+  }
 
-    if (left > 0 && A[left] == key) {
-        return left + 1;
-    }
-
-    else {
-        return -1;
-    }
+  if (left > 0 && A[left] == key) {
+    return left + 1;
+  } else {
+    return -1;
+  }
 }
 
 int main() {
-    freopen("binsearch.in", "r", stdin);
-    freopen("binsearch.out", "w", stdout);
+  freopen("binsearch.in", "r", stdin);
+  freopen("binsearch.out", "w", stdout);
 
-    int N = 0; cin >> N;
-    
-    int arr[N] = {0};
-    int counter = 0;
-    int funclast = 0;
-    int funcfirst = 0;
-    int arr_counting[N] = {0};
+  int N = 0;
+  cin >> N;
 
-    for(int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
+  int arr[N] = {
+    0
+  };
+  int counter = 0;
+  int funclast = 0;
+  int funcfirst = 0;
+  int arr_counting[N] = {
+    0
+  };
 
-    int M; cin >> M;
-    int arr_requests[M] = {0};
+  for (int i = 0; i < N; i++) {
+    cin >> arr[i];
+  }
 
-    for(int i = 0; i < M; i++) {
-        cin >> arr_requests[i];
-    }
+  int M;
+  cin >> M;
+  int arr_requests[M] = {
+    0
+  };
 
-    for(int i = 0; i < M; i++) {
-        counter = arr_requests[i];
-        funcfirst = myBinarySearch(arr, N, counter);
-        funclast = myBinarySearchLast(arr, N, counter);
+  for (int i = 0; i < M; i++) {
+    cin >> arr_requests[i];
+  }
 
-        cout << funcfirst << " " << funclast << endl;
+  for (int i = 0; i < M; i++) {
+    counter = arr_requests[i];
+    funcfirst = myBinarySearch(arr, N, counter);
+    funclast = myBinarySearchLast(arr, N, counter);
 
-    }
+    cout << funcfirst << " " << funclast << endl;
+
+  }
 }
