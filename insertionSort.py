3c5f9ba347f36360909c79bb7b92457b4c37e672
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2021-12-10 18:20:06 +0300

    UPD: insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -1,15 +1,15 @@
-fin = open("smallsort.in", "r")
-fout = open("smallsort.out", "w")
-N = int(fin.readline())
-A = list(map(int, fin.readline().split()))
+file_in = open("smallsort.in", "r")
+file_out = open("smallsort.out", "w")
+N = int(file_in.readline())
+array = list(map(int, file_in.readline().split()))
 
-for i in range(1, len(A)):
-    k = A[i]
+for i in range(1, len(array)):
+    k = array[i]
     j = i - 1
-    while k < A[j] and j >= 0:
-        A[j + 1] = A[j]
+    while k < array[j] and j >= 0:
+        array[j + 1] = array[j]
         j = j - 1
-    A[j + 1] = k
+    array[j + 1] = k
 
-print(*A, file = fout)
-fout.close()
+print(*array, file = file_out)
+file_out.close()
