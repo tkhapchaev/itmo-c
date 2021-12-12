27a23c3b1f553f99c5d8de0a7cf04bf9dd8851e6
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2021-12-12 11:12:07 +0300

    UPD: sortland.py

--- sortland.py
+++ sortland.py
@@ -1,32 +1,32 @@
 file_in = open("sortland.in", "r")
 file_out = open("sortland.out", "w")
 N = int(file_in.readline())
-A = list(map(float, file_in.readline().split()))
-B = []
+array_input = list(map(float, file_in.readline().split()))
+array_output = []
 
-for i in range(0, len(A)):
-    B.append(A[i])
-for i in range(1, len(A)):
-    key = A[i]
+for i in range(0, len(array_input)):
+    array_output.append(array_input[i])
+for i in range(1, len(array_input)):
+    key = array_input[i]
     j = i - 1
-    while key < A[j] and j >= 0:
-        A[j + 1] = A[j]
+    while key < array_input[j] and j >= 0:
+        array_input[j + 1] = array_input[j]
         j = j - 1
-    A[j + 1] = key
+    array_input[j + 1] = key
 
-richest = A[-1]
-poorest = A[0]
-average = A[len(A) // 2]
-for i in range(0, len(B)):
-    if B[i] == richest:
-        richestplace = i
-    if B[i] == poorest:
-        poorestplace = i
-    if B[i] == average:
-        averageplace = i
+richest = array_input[-1]
+poorest = array_input[0]
+average = array_input[len(array_input) // 2]
+for i in range(0, len(array_output)):
+    if array_output[i] == richest:
+        richest_index = i
+    if array_output[i] == poorest:
+        poorest_index = i
+    if array_output[i] == average:
+        average_index = i
 
-richestplace = str(richestplace + 1)
-poorestplace = str(poorestplace + 1)
-averageplace = str(averageplace + 1)
-file_out.write(poorestplace + " " + averageplace + " " + richestplace)
+richest_index = str(richest_index + 1)
+poorest_index = str(poorest_index + 1)
+average_index = str(average_index + 1)
+file_out.write(poorest_index + " " + average_index + " " + richest_index)
 file_out.close()
