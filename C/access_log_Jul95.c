b132455265a821a0912f42d0e9aa0a0ed65f3952
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2021-12-19 15:06:18 +0300

    UPD: access_log_Jul95.c

--- C/access_log_Jul95.c
+++ C/access_log_Jul95.c
@@ -19,7 +19,6 @@ QUEUE * push(QUEUE * __queue, int key) {
   NODE * newRequest = malloc(sizeof(NODE));
   newRequest -> value = key;
   newRequest -> next = NULL;
-
   if ((__queue -> head != NULL) && (__queue -> tail != NULL)) {
     __queue -> tail -> next = newRequest;
     __queue -> tail = newRequest;
@@ -32,7 +31,6 @@ QUEUE * push(QUEUE * __queue, int key) {
 
 int show_head(QUEUE * __queue) {
   int key = 0;
-
   if ((__queue -> head != NULL) && (__queue -> tail != NULL)) {
     key = __queue -> head -> value;
   }
@@ -43,7 +41,6 @@ int show_head(QUEUE * __queue) {
 int pop(QUEUE * __queue) {
   int key = 0;
   NODE * oldRequest;
-
   if (__queue -> head != NULL) {
     oldRequest = __queue -> head;
     key = oldRequest -> value;
@@ -62,7 +59,6 @@ int main() {
   QUEUE requests;
   requests.head = NULL;
   requests.tail = NULL;
-
   int max = 0;
   int line = 0;
   int seconds = 0;
@@ -71,21 +67,17 @@ int main() {
   int toCountLines = 1;
   int toCountErrors = 0;
   int toCountRequests = 0;
-
   int maxDateTo[4] = {
     0
   };
   int maxDateFrom[4] = {
     0
   };
-
   char symbol;
   char string[550];
-
   char * toFind;
   char * bracketPosition;
   char * __pointToString;
-
   char ERROR_A[10] = "\" 500 ";
   char ERROR_B[10] = "\" 501 ";
   char ERROR_C[10] = "\" 502 ";
@@ -97,12 +89,11 @@ int main() {
   char ERROR_I[10] = "\" 508 ";
   char ERROR_J[10] = "\" 510 ";
   char ERROR_K[10] = "\" 511 ";
-
   while ((symbol = fgetc(file_in)) != EOF) {
     if (symbol == '\n') toCountLines++;
   }
-  fseek(file_in, 0, 0);
 
+  fseek(file_in, 0, 0);
   scanf("%d", & seconds);
   if (seconds <= 0) {
     printf("-----------------------------------------------------------------------------------------------------------------------\n");
@@ -113,7 +104,6 @@ int main() {
   printf("-----------------------------------------------------------------------------------------------------------------------\n");
   printf("5XX -- Server errors -- list: \n");
   printf("-----------------------------------------------------------------------------------------------------------------------\n");
-
   for (int i = 0; i < toCountLines; i++) {
     int __flag = 0;
     __pointToString = fgets(string, 550, file_in);
@@ -128,7 +118,6 @@ int main() {
     if ((toFind = strstr(__pointToString, ERROR_I)) != NULL) __flag++;
     if ((toFind = strstr(__pointToString, ERROR_J)) != NULL) __flag++;
     if ((toFind = strstr(__pointToString, ERROR_K)) != NULL) __flag++;
-
     if (__flag > 0) {
       if (i != toCountLines - 1) {
         toCountErrors++;
@@ -141,52 +130,41 @@ int main() {
       }
     }
   }
-  fseek(file_in, 0, 0);
 
+  fseek(file_in, 0, 0);
   printf("-----------------------------------------------------------------------------------------------------------------------\n");
   printf("5XX -- Server errors -- counted: %d \n", toCountErrors);
   printf("-----------------------------------------------------------------------------------------------------------------------\n");
-
   if (__continue != 0) {
     for (int i = 0; i < toCountLines; i++) {
       __pointToString = fgets(string, 550, file_in);
       bracketPosition = strchr(__pointToString, '[');
-
       if (bracketPosition != NULL) {
         line++;
         int bracketPositionInt = bracketPosition - __pointToString;
-
         int dayTens = string[bracketPositionInt + 1] - '0';
         int dayUnits = string[bracketPositionInt + 2] - '0';
         int daySummary = 10 * dayTens + dayUnits;
-
         int hoursTens = string[bracketPositionInt + 13] - '0';
         int hoursUnits = string[bracketPositionInt + 14] - '0';
         int hoursSummary = 10 * hoursTens + hoursUnits;
-
         int minutesTens = string[bracketPositionInt + 16] - '0';
         int minutesUnits = string[bracketPositionInt + 17] - '0';
         int minutesSummary = 10 * minutesTens + minutesUnits;
-
         int secondsTens = string[bracketPositionInt + 19] - '0';
         int secondsUnits = string[bracketPositionInt + 20] - '0';
         int secondsSummary = 10 * secondsTens + secondsUnits;
-
         int result = (daySummary * 86400 + hoursSummary * 3600 + minutesSummary * 60 + secondsSummary) - 86400;
         if (i == 0) push( & requests, result);
-
         else if (i != 0) {
           push( & requests, result);
           toCountRequests++;
-
           if (toCountRequests > max) {
             max = toCountRequests;
-
             maxDateFrom[0] = ((show_head( & requests) / 86400) + 1);
             maxDateFrom[1] = ((show_head( & requests) % 86400) / 3600);
             maxDateFrom[2] = (((show_head( & requests) % 86400) % 3600) / 60);
             maxDateFrom[3] = ((((show_head( & requests) % 86400) % 3600) % 60));
-
             maxDateTo[0] = daySummary;
             maxDateTo[1] = hoursSummary;
             maxDateTo[2] = minutesSummary;
