dd8a612e33a7ffd96f3c152ddf957bd708e6c666
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2021-12-29 21:21:32 +0300

    Updated queue.cpp

--- Algorithms and Data structures/queue.cpp
+++ Algorithms and Data structures/queue.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#define SIZE_Q 1000000
+#define _QUEUE_SIZE 1000000
 using namespace std;
 
 typedef struct myQueue {
-  int maxQueue[SIZE_Q];
+  int maxQueue[_QUEUE_SIZE];
   int prev = 0;
   int next = 1;
 }
 myQueue;
 
 void addToQueue(myQueue * Queue, int value) {
-  if (Queue -> prev < SIZE_Q - 1) {
+  if (Queue -> prev < _QUEUE_SIZE - 1) {
     Queue -> prev += 1;
     Queue -> maxQueue[Queue -> prev] = value;
   }
@@ -25,16 +25,13 @@ int deleteFromQueue(myQueue * Queue) {
 int main() {
   cin.tie(0);
   ios_base::sync_with_stdio(0);
-
   freopen("queue.in", "r", stdin);
   freopen("queue.out", "w", stdout);
-
   int N;
   cin >> N;
   int number = 0;
   char sign;
   myQueue * Q = new myQueue[sizeof(myQueue * )];
-
   for (int i = 0; i < N; i++) {
     cin >> sign;
 
