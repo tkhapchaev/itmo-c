8bcaa2ad209cd1266ec0d5a39163fa772f7e0782
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2021-12-29 16:47:39 +0300

    Updated life.c

--- C/life.c
+++ C/life.c
@@ -1,68 +1,69 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <direct.h>
-#include <stdlib.h>
-#define TWO_BYTES unsigned short
-#define FOUR_BYTES unsigned long
-
+#define BYTE unsigned char
+#define WORD uint16_t
+#define DWORD uint32_t
 #pragma pack(push, 1)
 typedef struct bitmap_header {
-  TWO_BYTES Type;
-  FOUR_BYTES Size;
-  TWO_BYTES Reserved_1;
-  TWO_BYTES Reserved_2;
-  FOUR_BYTES OffsetBits;
+  WORD type;
+  DWORD size;
+  WORD reserved_1;
+  WORD reserved_2;
+  DWORD offsetBits;
 }
-BITMAP_FILE_HEADER;
+bitmap_file_header_structure;
 
 #pragma pack(pop)
 typedef struct bitmap_info {
-  FOUR_BYTES Size;
-  FOUR_BYTES Width;
-  FOUR_BYTES Height;
-  TWO_BYTES Planes;
-  TWO_BYTES BitCount;
-  FOUR_BYTES Compression;
-  FOUR_BYTES SizeImage;
-  FOUR_BYTES XPixelsPerMeter;
-  FOUR_BYTES YPixelsPerMeter;
-  FOUR_BYTES ColorUsed;
-  FOUR_BYTES ColorImportant;
+  DWORD size;
+  DWORD width;
+  DWORD height;
+  WORD planes;
+  WORD bitCount;
+  DWORD compression;
+  DWORD imageSize;
+  DWORD XPixelsPerMeter;
+  DWORD YPixelsPerMeter;
+  DWORD colorsUsed;
+  DWORD colorsImportant;
 }
-BITMAP_INFO_HEADER;
+bitmap_info_header_structure;
 
 int count_neighbours(char ** data, int current_position_x, int current_position_y, int max_height, int max_width) {
   int neighbours = 0;
-  int RI = current_position_x + 1;
-  int RD = current_position_x - 1;
-  int CI = current_position_y + 1;
-  int CD = current_position_y - 1;
-  if (RI == max_height)
-    RI = 0;
-  if (RD == -1)
-    RD = max_height - 1;
-  if (CI == max_width)
-    CI = 0;
-  if (CD == -1)
-    CD = max_width - 1;
-  neighbours += data[RD][CD] == 1;
-  neighbours += data[RD][current_position_y] == 1;
-  neighbours += data[RD][CI] == 1;
-  neighbours += data[current_position_x][CD] == 1;
-  neighbours += data[current_position_x][CI] == 1;
-  neighbours += data[RI][CD] == 1;
-  neighbours += data[RI][current_position_y] == 1;
-  neighbours += data[RI][CI] == 1;
+  int ri = current_position_x + 1;
+  int rd = current_position_x - 1;
+  int ci = current_position_y + 1;
+  int cd = current_position_y - 1;
+  if (ri == max_height)
+    ri = 0;
+  if (rd == -1)
+    rd = max_height - 1;
+  if (ci == max_width)
+    ci = 0;
+  if (cd == -1)
+    cd = max_width - 1;
+  neighbours += data[rd][cd] == 1;
+  neighbours += data[rd][current_position_y] == 1;
+  neighbours += data[rd][ci] == 1;
+  neighbours += data[current_position_x][cd] == 1;
+  neighbours += data[current_position_x][ci] == 1;
+  neighbours += data[ri][cd] == 1;
+  neighbours += data[ri][current_position_y] == 1;
+  neighbours += data[ri][ci] == 1;
   return neighbours;
 }
 
 int main(int argc, char ** argv) {
-  unsigned char out_filename[128];
+  BYTE out_filename[128];
   int next_dump = 0;
   char palette[8];
   int palette_size;
-  BITMAP_FILE_HEADER bitmap_header;
-  BITMAP_INFO_HEADER bitmap_info;
+  bitmap_file_header_structure bmp_header;
+  bitmap_info_header_structure bmp_info;
   char input[128] = {
     0
   };
@@ -102,28 +103,28 @@ int main(int argc, char ** argv) {
   _mkdir(output);
   file_in = fopen(input, "rb");
   if (file_in == NULL) return NULL;
-  if (fread( & bitmap_header, 1, sizeof(bitmap_header), file_in) != sizeof(bitmap_header)) {
+  if (fread( & bmp_header, 1, sizeof(bmp_header), file_in) != sizeof(bmp_header)) {
     fclose(file_in);
     return NULL;
   }
 
-  if (bitmap_header.Type != 0x4D42) {
+  if (bmp_header.type != 0x4d42) {
     printf("Error :: %s is not a .bmp file.\n", input);
     fclose(file_in);
     return NULL;
   }
 
-  fread( & bitmap_info, 1, sizeof(BITMAP_INFO_HEADER), file_in) != sizeof(BITMAP_INFO_HEADER);
-  palette_size = bitmap_header.OffsetBits - ftell(file_in);
+  fread( & bmp_info, 1, sizeof(bitmap_info_header_structure), file_in) != sizeof(bitmap_info_header_structure);
+  palette_size = bmp_header.offsetBits - ftell(file_in);
   fread(palette, 1, palette_size, file_in);
-  if (bitmap_info.Compression != 0) {
+  if (bmp_info.compression != 0) {
     printf("Error :: compression is not supported.\n");
     fclose(file_in);
     return NULL;
   }
 
-  int current_string_length = (bitmap_header.Size - bitmap_header.OffsetBits) / bitmap_info.Height;
-  if (bitmap_info.BitCount != 1) {
+  int current_string_length = (bmp_header.size - bmp_header.offsetBits) / bmp_info.height;
+  if (bmp_info.bitCount != 1) {
     printf("Error :: not a monochrome image.\n");
     fclose(file_in);
     return NULL;
@@ -131,16 +132,16 @@ int main(int argc, char ** argv) {
   }
 
   current_row = malloc(current_string_length);
-  data = malloc(bitmap_info.Height * sizeof(int * ));
-  previous = malloc(bitmap_info.Height * sizeof(int * ));
-  temporary = malloc(bitmap_info.Height * sizeof(int * ));
-  for (int i = 0; i < bitmap_info.Height; i++) {
-    data[i] = malloc(bitmap_info.Width);
-    previous[i] = calloc(bitmap_info.Width, 1);
-    temporary[i] = malloc(bitmap_info.Width);
-    fseek(file_in, bitmap_header.OffsetBits + (bitmap_info.Height - i - 1) * current_string_length, SEEK_SET);
+  data = malloc(bmp_info.height * sizeof(int * ));
+  previous = malloc(bmp_info.height * sizeof(int * ));
+  temporary = malloc(bmp_info.height * sizeof(int * ));
+  for (int i = 0; i < bmp_info.height; i++) {
+    data[i] = malloc(bmp_info.width);
+    previous[i] = calloc(bmp_info.width, 1);
+    temporary[i] = malloc(bmp_info.width);
+    fseek(file_in, bmp_header.offsetBits + (bmp_info.height - i - 1) * current_string_length, SEEK_SET);
     fread(current_row, 1, current_string_length, file_in);
-    for (int j = 0; j < bitmap_info.Width; j++) {
+    for (int j = 0; j < bmp_info.width; j++) {
       data[i][j] = (current_row[j / 8] >> (7 - j % 8)) & 1;
     }
   }
@@ -148,31 +149,31 @@ int main(int argc, char ** argv) {
   fclose(file_in);
   int current_iteration = 0;
   while (max_iterations == -1 || current_iteration < max_iterations) {
+    unsigned int counter;
     unsigned int j;
     unsigned int k;
-    unsigned int counter;
-    for (j = 0; j < bitmap_info.Height && memcmp(data[j], previous[j], bitmap_info.Width) == 0; j++);
-    if (j == bitmap_info.Height) {
+    for (j = 0; j < bmp_info.height && memcmp(data[j], previous[j], bmp_info.width) == 0; j++);
+    if (j == bmp_info.height) {
       break;
     }
 
-    for (j = 0, k = bitmap_info.Width; j < bitmap_info.Height && k == bitmap_info.Width; j++)
-      for (k = 0; k < bitmap_info.Width && data[j][k] == ' '; k++);
+    for (j = 0, k = bmp_info.width; j < bmp_info.height && k == bmp_info.width; j++)
+      for (k = 0; k < bmp_info.width && data[j][k] == ' '; k++);
 
-    if (j == bitmap_info.Height) {
+    if (j == bmp_info.height) {
       break;
     }
 
     if (current_iteration == next_dump) {
       sprintf(out_filename, "%s\\%06d.bmp", output, current_iteration);
       file_in = fopen(out_filename, "wb");
-      fwrite( & bitmap_header, 1, sizeof(bitmap_header), file_in);
-      fwrite( & bitmap_info, 1, sizeof(bitmap_info), file_in);
+      fwrite( & bmp_header, 1, sizeof(bmp_header), file_in);
+      fwrite( & bmp_info, 1, sizeof(bmp_info), file_in);
       fwrite(palette, 1, palette_size, file_in);
-      for (int i = 0; i < bitmap_info.Height; i++) {
+      for (int i = 0; i < bmp_info.height; i++) {
         memset(current_row, 0, current_string_length);
-        for (int j = 0; j < bitmap_info.Width; j++) {
-          current_row[j / 8] |= data[bitmap_info.Height - 1 - i][j] << (7 - j % 8);
+        for (int j = 0; j < bmp_info.width; j++) {
+          current_row[j / 8] |= data[bmp_info.height - 1 - i][j] << (7 - j % 8);
         }
 
         fwrite(current_row, 1, current_string_length, file_in);
@@ -182,10 +183,10 @@ int main(int argc, char ** argv) {
       next_dump += dump_frequency;
     }
 
-    for (j = 0; j < bitmap_info.Height; j++) memcpy(previous[j], data[j], bitmap_info.Width);
-    for (j = 0; j < bitmap_info.Height; j++) {
-      for (k = 0; k < bitmap_info.Width; k++) {
-        counter = count_neighbours(data, j, k, bitmap_info.Height, bitmap_info.Width);
+    for (j = 0; j < bmp_info.height; j++) memcpy(previous[j], data[j], bmp_info.width);
+    for (j = 0; j < bmp_info.height; j++) {
+      for (k = 0; k < bmp_info.width; k++) {
+        counter = count_neighbours(data, j, k, bmp_info.height, bmp_info.width);
         if (data[j][k] == 0) {
           if (counter == 3)
             temporary[j][k] = 1;
@@ -202,11 +203,11 @@ int main(int argc, char ** argv) {
       }
     }
 
-    for (j = 0; j < bitmap_info.Height; j++) memcpy(data[j], temporary[j], bitmap_info.Width);
+    for (j = 0; j < bmp_info.height; j++) memcpy(data[j], temporary[j], bmp_info.width);
     current_iteration++;
   }
 
-  for (int i = 0; i < bitmap_info.Height; i++) {
+  for (int i = 0; i < bmp_info.height; i++) {
     free(data[i]);
     free(previous[i]);
     free(temporary[i]);
