40ad26279f15fb0cb3735b81009a7cc2e692ae4e
Timur Khapchaev <89847233+tkhapchaev@users.noreply.github.com>
2022-02-04 11:30:36 +0300

    Deleted Algorithms and Data structures directory

--- Algorithms and Data structures/aplusb.py
+++
@@ -1,5 +0,0 @@
-file_in = open("aplusb.in", "r")
-file_out = open("aplusb.out", "w")
-a, b = map(int, file_in.readline().split())
-print(a + b, file = file_out)
-file_out.close()
--- Algorithms and Data structures/aplusbb.py
+++
@@ -1,5 +0,0 @@
-file_in = open("aplusbb.in", "r")
-file_out = open("aplusbb.out", "w")
-a, b = map(int, file_in.readline().split())
-print(a + b * b, file = file_out)
-file_out.close()
